4b9d267d9feb624e7362685cdcf09df23daa58a8
JhoselPR <112789135+JhoselPR@users.noreply.github.com>
2025-02-03 23:58:33 -0600

    router update

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { DataProvider } from './context/DataContext.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { DataProvider } from './context/DataContext.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <App />
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
